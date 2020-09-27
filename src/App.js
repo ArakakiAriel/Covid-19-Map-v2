f7bcb2ed7e6daf335fb141bc6e1154c62751f8a5
ArielArakaki <arakaki.arielkenji@gmail.com>
2020-09-28 00:31:05 +0900

    Se agrega react al proyecto

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>COVID-19 MAP TRACKER</h1>
+    </div>
+  );
+}
+
+export default App;
